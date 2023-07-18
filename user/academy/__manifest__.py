10d31bc9bef110f6c025ed19e498f3cfdd665b7d
LDLV <ldlv@odoo.com>
2023-07-18 14:16:34 -0600

    [ADD] New Module

---
+++ user/academy/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Odoo Academy',
+    'summary': """ Module to handle Course and Sessions""",
+    'description': """Module to handle:
+        - Courses
+        - Sessions
+        - Atendees
+        """,
+    'license': 'OPL-1',
+    'author': 'ldlv',
+    'website': 'www.odoo.com',
+    'category': 'Custom Modules/Tech Training',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
